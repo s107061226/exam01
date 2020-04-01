59f393edc286b785827235f8129631b4bf7c4832
johnny <johnnyjohnny6201@gmail.com>
2020-04-01 17:32:28 +0800

    Q3_finish

--- Q2/main.cpp
+++ Q2/main.cpp
@@ -2,9 +2,12 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2);
+PwmOut PWM1(D6);
 
 int main()
 {
+    float i, j;
+
     // Q2
     uLCD.cls();
     uLCD.background_color(0xFFFFFF);
@@ -18,6 +21,19 @@ int main()
     uLCD.line(40, 80, 80, 80, 0x0000FF);
     uLCD.line(80, 40, 80, 80, 0x0000FF);
     wait(1);
+
+    // Q3
+    i = 0;
+    j = 0.1;
+    PWM1.period(0.001);
+    PWM1 = 0;
+    while (true) {
+        if (i >= 1) j = -0.1;
+        if (i <= 0) j = 0.1;
+        PWM1 = i;
+        wait(0.1);
+        i = i + j;
+    }
 
 
 }
